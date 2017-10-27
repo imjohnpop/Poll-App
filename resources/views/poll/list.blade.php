0a0456b34b99bd385f70aef4886f162fa1444a85
Elena Porras <m.elenaporras@hotmail.com>
2017-10-27 03:03:17 -0600

    looking for conflicts where there are not. Hi btw

--- resources/views/poll/list.blade.php
+++ resources/views/poll/list.blade.php
@@ -5,6 +5,30 @@
 @endsection
 
 @section('content')
-Hello modafoca
-
+<main>
+    <h1>Poll list</h1>
+    <section class="container">
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <button>Add</button>
+            </div>
+            <div class="col-2"></div>        
+        </div>
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8 public_poll">
+                <h2>How much do you like cookies?</h2>
+                <form action="" method="post">
+                    <input type="radio">An f*ing lot. <br>
+                    <input type="radio">Just a lot.<br>
+                    <input type="radio">Eh, I'm a boring indiferent person.<br>
+                    <input type="radio">What is a cookie?<br>
+                    <input type="submit" value="Vote">
+                </form>
+            </div>
+            <div class="col-2"></div>        
+        </div>
+    </section>
+</main>
 @endsection
