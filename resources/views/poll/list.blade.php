4038b4038a5b8390410ab271a3c556c7292d53a0
Elena Porras <m.elenaporras@hotmail.com>
2017-10-27 07:07:08 -0600

    fixing stuff

--- resources/views/poll/list.blade.php
+++ resources/views/poll/list.blade.php
@@ -16,32 +16,19 @@
             </div>
             <div class="col-2"></div>        
         </div>
-        <div class="row" style="display:none">
-            <div class="col-2"></div>
-            <div class="col-8">
-                <form action="" method="post">
-                    <label for="title">Poll name:</label>
-                    <input type="text"><br>
-                    <label>Choice text:</label>
-                    <input type="text"><br>
-                    <input type="submit" value="Add poll">
-                </form>
-            </div>
-            <div class="col-2"></div>        
-        </div>
         @foreach($polls as $poll)
         <div class="row">
             <div class="col-2"></div>
             <div class="col-8 public_poll">
                 <h2>{{ $poll->poll_name}}</h2>
                 <form action="" method="post">
-                @if($poll->nr_choice === 0)
+                @if($poll->nr_choice === 1)
                     <?php $choices=\App\Choices::where('choice_to_poll', '=', $poll->poll_id)->get();?>
                     @foreach($choices as $choice)
                         <input type="checkbox">{{ $choice->choice_text }}<br>
                     @endforeach
                 @endif
-                @if($poll->nr_choice === 1)
+                @if($poll->nr_choice === 0)
                     @foreach($choices as $choice)
                         <input type="radio">{{ $choice->choice_text }}<br>
                     @endforeach
