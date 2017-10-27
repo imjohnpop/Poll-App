2d6bf027fd3ebba231ca83dfbd832de031f5782b
imjohnpop <poprocsi.j@gmail.com>
2017-10-27 13:24:28 +0200

    working DB

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         
         Votes::create([
             'user_id' => 1,
-            'vote_to_poll' => 2
+            'vote_to_poll' => 1
         ]);
 
         Choices::create([
