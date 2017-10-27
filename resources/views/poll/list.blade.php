bac778201a3da168196eb19e7fe9e5a61d53823d
Elena Porras <m.elenaporras@hotmail.com>
2017-10-27 05:27:37 -0600

    solving conflict

--- resources/views/poll/list.blade.php
+++ resources/views/poll/list.blade.php
@@ -15,16 +15,35 @@
             </div>
             <div class="col-2"></div>        
         </div>
-
+        <div class="row" style="display:none">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <form action="" method="post">
+                    <label for="title">Poll name:</label>
+                    <input type="text"><br>
+                    <label>Choice text:</label>
+                    <input type="text"><br>
+                    <input type="submit" value="Add poll">
+                </form>
+            </div>
+            <div class="col-2"></div>        
+        </div>
         @foreach($polls as $poll)
         <div class="row">
             <div class="col-2"></div>
             <div class="col-8 public_poll">
                 <h2>{{ $poll->poll_name}}</h2>
                 <form action="" method="post">
-                @foreach($choices as $choice)
-                    <input type="radio">{{ $choice->text }}<br>
-                @endforeach
+                @if($poll->nr_choice === 0)
+                    @foreach($choices as $choice)
+                        <input type="checkbox">{{ $choice->text }}<br>
+                    @endforeach
+                @endif
+                @if($poll->nr_choice === 1)
+                    @foreach($choices as $choice)
+                        <input type="radio">{{ $choice->text }}<br>
+                    @endforeach
+                @endif
                     <input type="submit" value="Vote">
                 </form>
             </div>
