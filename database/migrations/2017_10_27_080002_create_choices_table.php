c1e3892bc8545d3f204cf077ceb19d71bbd11b1a
Elena Porras <m.elenaporras@hotmail.com>
2017-10-27 02:14:04 -0600

    Modals and migrations created for tables

---
+++ database/migrations/2017_10_27_080002_create_choices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->increments('choices_id');
+            $table->string('text');
+            $table->integer('poll_id');
+            $table->integer('nr_votes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choices');
+    }
+}
