ce84041d5a6836eb0b96b5c0a5636c7d6620af47
Elena Porras <m.elenaporras@hotmail.com>
2017-10-29 12:43:42 -0600

    FINALLY WORKS

--- app/Poll.php
+++ app/Poll.php
@@ -23,4 +23,22 @@ class Poll extends Model
     public function user(){
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function voted($current){
+        $id = $this->poll_id;
+        $answered=\App\Votes::where('user_id', '=', $current)->get();
+        $ids = [];
+
+        foreach ($answered as $val)
+        {
+            $ids[] = $val->vote_to_poll;
+        }
+
+        if(in_array($id, $ids))
+        {
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
--- resources/views/poll/list.blade.php
+++ resources/views/poll/list.blade.php
@@ -39,22 +39,11 @@
             </div>
             <div class="col-2"></div>
         </div>
+        <?php
+            $current=Illuminate\Support\Facades\Auth::user()->id;
+        ?>
         @foreach($polls as $poll)
-            <?php
-                $id = $poll->poll_id;
-                $current=Illuminate\Support\Facades\Auth::user()->id;
-                $answered=\App\Votes::where('user_id', '=', $current)->get();
-                foreach ($answered as $val)
-                {
-                    if($val->vote_to_poll == $id)
-                    {
-                        $answer = false;
-                    }else{
-                        $answer = true;
-                    }
-                }
-            ?>
-            @if($answer)
+            @if($poll->voted($current))
                 <div class="row">
                     <div class="col-2"></div>
                     <div class="col-8">
@@ -92,9 +81,9 @@
                             <?php $votes[]= $choice->nr_votes?>
                         @endforeach
                         <?php $nr_votes = array_sum($votes);?>
-                        <div class="card mt-3">
+                        <div class="card mt-3 poll-shadow poll-card">
                             <div class="card-header d-flex justify-content-between py-2">
-                                <h4 class="text-center"><a class="text-dark" href="{{action('listController@view', ["idcko" => "$poll->poll_id"])}}">{{ $poll->poll_name }}</a></h4>
+                                <h4 class=" py-1">{{ $poll->poll_name }}</h4>
                                 <span class="badge badge-primary"><h5 class="pt-1">{{ round($nr_votes) }} votes</h5></span>
                             </div>
                             @foreach($choices as $choice)
